2ffea85605628d0acbcf61630308c980fa3d27e5
sagar <sagar@192.168.0.5>
2017-06-18 23:18:20 +0530

    student12 application

---
+++ src/student12/Student12.java
@@ -0,0 +1,20 @@
+package student12;
+
+public class Student12
+{
+	private int sno;
+
+	@Override
+	public String toString() {
+		return "Student12 [sno=" + sno + "]";
+	}
+
+	public int getSno() {
+		return sno;
+	}
+
+	public void setSno(int sno) {
+		this.sno = sno;
+	}
+
+}
